890d209c85a49537365808bc14f082493484eca1
UNHOT <Serverings@protonmail.com>
2023-07-16 13:56:38 +0330

    Fix background images.

--- components/AerialViewBusinessTeam.tsx
+++ components/AerialViewBusinessTeam.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
 
 export default function AerialViewBusinessTeam() {
   return (
-    <div className="w-[1440px] h-[598px] bg-black bg-opacity-40 backdrop-blur-[10px]">
-      <div className="text-center">
+    <div
+      style={{ backgroundImage: "url('/static/images/Office.png')" }}
+      className="w-[1440px] h-[598px] bg-black bg-opacity-40 backdrop-blur-[10px]"
+    >
+      <div className="text-center py-5">
         <span className="text-white text-xl font-normal tracking-[7px]">
           Landa Holding
           <br />
@@ -12,7 +16,7 @@ export default function AerialViewBusinessTeam() {
           What Our Cooperation Members Say?
         </span>
       </div>
-      <div className="w-[1006px] h-[380px] relative bg-neutral-500 bg-opacity-60">
+      <div className="w-[1006px] h-[380px] relative bg-neutral-500 bg-opacity-60 mx-auto">
         <div className="left-[416px] top-[316px] absolute justify-start items-start gap-[19px] inline-flex">
           <div className="w-[15px] h-[15px] bg-stone-500 rounded-full" />
           <div className="w-[15px] h-[15px] rounded-full border border-white" />
@@ -29,18 +33,21 @@ export default function AerialViewBusinessTeam() {
           </div>
         </div>
         <div className="left-[66px] top-[23px] absolute justify-start items-center gap-[13px] inline-flex">
-          <img
+          <Image
             className="w-[108px] h-[104px] border"
             src="https://via.placeholder.com/108x104"
+            width={500}
+            height={500}
+            alt='Avatar'
           />
           <div className="h-[42px] flex-col justify-center items-start gap-[3px] inline-flex">
-            <div className="w-[135px] h-[21px] text-white text-xl font-medium">
+            <div className=" h-[21px] text-white text-xl font-medium">
               Morteza Jafari
             </div>
-            <div className="w-[179px] h-[18px] text-white text-base font-normal">
+            <div className=" h-[18px] text-white text-base font-normal">
               Founder of IRimmigration
             </div>
-            <div className="w-[162px] h-[18px] text-white text-[15px] font-normal">
+            <div className="h-[18px] text-white text-[15px] font-normal">
               Investor
             </div>
           </div>
