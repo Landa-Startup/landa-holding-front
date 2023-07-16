8501c54d85b67f484815fd23c53de86d3b53c1ac
UNHOT <Serverings@protonmail.com>
2023-07-16 13:57:26 +0330

    Make year dynamic.

--- components/CopyRight.tsx
+++ components/CopyRight.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function CopyRight() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="left-[599.50px] top-[350px] absolute text-center text-white text-base font-normal">
-      © Copyright 2023 by LandaHolding
+      © {currentYear} by Landa Holding
     </div>
   );
 }
