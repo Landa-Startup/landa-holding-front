6c4b840f4329c4da70991eb8a859c15ad754f5ea
UNHOT <Serverings@protonmail.com>
2023-07-16 10:40:24 +0330

    Init pages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import type { Metadata } from 'next';
-import HomePage, { Joke } from './HomePage';
+import Hero from '@/components/Hero'
+import React from 'react'
 
-const title = 'Home | nextjs-starter';
-
-export const metadata: Metadata = {
-  title: title,
-};
-
-async function getJokes(): Promise<Joke[]> {
-  const res = await fetch('https://official-joke-api.appspot.com/jokes/ten');
-  const jokes: Joke[] = await res.json();
-  return jokes;
-}
-
-export default async function Home() {
-  const jokes = await getJokes();
-  return <HomePage jokes={jokes} />;
+export default function page() {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  )
 }
