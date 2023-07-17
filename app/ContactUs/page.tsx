0dfeb0e574f38145210adec09f5f0a3bd52d68fa
SamanKchn <samankarchegani@gmail.com>
2023-07-17 17:12:13 +0330

    Contact Us pt1

--- app/ContactUs/page.tsx
+++ app/ContactUs/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import React from 'react';
+import Hero from '@/components/Hero';
+import ContactUsForm from '@/components/ContactUs/ContactUsForm';
+import ContactUsDescription from '@/components/ContactUs/ContactUsDescription';
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <Hero></Hero>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 container mx-auto px-3 my-20">
+        <div>
+          <ContactUsDescription/>
+        </div>
+        <div>
+          <ContactUsForm/>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ components/ContactUs/ContactUsForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ContactUsForm() {
+  return (
+    <div>
+      <h2 className="text-5xl font-light">Get in touch</h2>
+    </div>
+  );
+}
