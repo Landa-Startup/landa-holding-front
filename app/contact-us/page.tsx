c86b46b03bddbe0f2a1e915c321216036480809f
SamanKchn <samankarchegani@gmail.com>
2023-07-22 18:21:33 +0330

    style: App/contact-us

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -5,14 +5,10 @@ import ContactUsDescription from '@/components/contact-us/ContactUsDescription';
 export default function page() {
   return (
     <div>
-      <Hero></Hero>
+      <Hero/>
       <div className="container grid grid-cols-1 gap-5 px-3 mx-auto my-20 md:grid-cols-2">
-        <div>
           <ContactUsDescription/>
-        </div>
-        <div>
           <ContactUsForm/>
-        </div>
       </div>
     </div>
   );
