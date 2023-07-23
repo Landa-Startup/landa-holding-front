23ff8c02142fcac9b940d028b10a09bfae6b8bf6
UNHOT <Serverings@protonmail.com>
2023-07-23 13:05:31 +0330

    Fix: Put components in loop.

--- components/LandaMagazine.tsx
+++ components/LandaMagazine.tsx
@@ -2,11 +2,32 @@ import React from 'react';
 import LandaMagazineCard from './LandaMagazineCard';
 
 export default function LandaMagazine() {
+  const cardData = [
+    {
+      title: 'Benefits of Spa Treatments',
+      image: '/static/images/annie-spratt-hWJsOnaWTqs-unsplash 1.png',
+      type: 'Acceleration',
+      date: 'apr 27',
+    },
+    {
+      title: 'Fitness Center',
+      image: '/static/images/remy_loz-3S0INpfREQc-unsplash (1) 1.png',
+      type: 'Investment',
+      date: 'apr 27',
+    },
+    {
+      title: 'Benefits of Spa Treatments',
+      image: '/static/images/emile-perron-xrVDYZRGdw4-unsplash 1.png',
+      type: 'Investment',
+      date: 'apr 27',
+    },
+  ];
+
   return (
     <div>
       <div className="h-[884px] relative bg-neutral-800 flex justify-center">
         <div className="w-[1362px] h-[666px] left-[45px] top-[73px] absolute">
-          <div className="w-[613px] left-[76px] top-0 absolute text-white text-[64px] font-normal">
+          <div className="w-[613px] left-[76px] top-0 absolute text-white text-[64px] font-normal font-gilda">
             LANDA MAGAZINE
           </div>
           <div className="flex flex-row items-center justify-center absolute top-[150px] left-10">
@@ -26,24 +47,15 @@ export default function LandaMagazine() {
               </svg>
             </button>
             <div className="grid grid-cols-3 gap-4">
-              <LandaMagazineCard
-                title="Benefits of Spa Treatments"
-                image="/static/images/annie-spratt-hWJsOnaWTqs-unsplash 1.png"
-                type="Acceleration"
-                date="apr 27"
-              />
-              <LandaMagazineCard
-                title="Fitness Center"
-                image="/static/images/remy_loz-3S0INpfREQc-unsplash (1) 1.png"
-                type="Investment"
-                date="apr 27"
-              />
-              <LandaMagazineCard
-                title="Benefits of Spa Treatments"
-                image="/static/images/emile-perron-xrVDYZRGdw4-unsplash 1.png"
-                type="Investment"
-                date="apr 27"
-              />
+              {cardData.map((data, index) => (
+                <LandaMagazineCard
+                  key={index}
+                  title={data.title}
+                  image={data.image}
+                  type={data.type}
+                  date={data.date}
+                />
+              ))}
             </div>
             <button className="border border-white rounded-full ml-3 w-12 h-12 flex items-center justify-center">
               {/* Right Arrow */}
@@ -62,8 +74,8 @@ export default function LandaMagazine() {
             </button>
           </div>
         </div>
-        <div className="w-[223px] h-[57px] py-3.5 bg-[#AA8453] justify-center items-center gap-2.5 inline-flex absolute bottom-8 ">
-          <div className="text-white text-xl font-medium tracking-wide">
+        <div className="w-[223px] h-[57px] py-3.5 bg-primary justify-center items-center gap-2.5 inline-flex absolute bottom-8 ">
+          <div className="text-white text-xl font-medium tracking-wide font-condensed">
             Read MORE
           </div>
         </div>
