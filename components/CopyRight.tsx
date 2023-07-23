1c02b40366aeeddbf267e6b5f12f9c32389efe0a
UNHOT <Serverings@protonmail.com>
2023-07-23 13:09:06 +0330

    Chore: Make better.

--- components/CopyRight.tsx
+++ components/CopyRight.tsx
@@ -4,8 +4,9 @@ export default function CopyRight() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="w-full flex justify-center top-[350px] absolute text-center text-white text-base font-normal">
-      © {currentYear} by Landa Holding
+    <div className="w-full flex justify-center top-[350px] absolute text-center text-white text-base font-normal font-barlow">
+      Copyright © {currentYear} Landa ® , All
+      Rights Reserved.
     </div>
   );
 }
