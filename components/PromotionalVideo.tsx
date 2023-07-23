4b06d20307830fb1f5882e4e81749a57e277159b
UNHOT <Serverings@protonmail.com>
2023-07-23 13:11:23 +0330

    Chore: Minor fixes.

--- components/PromotionalVideo.tsx
+++ components/PromotionalVideo.tsx
@@ -7,20 +7,21 @@ export default function PromotionalVideo() {
         backgroundImage: "url('/static/images/blurred-co-working.png')",
         backgroundPosition: 'center',
         backgroundSize: 'cover',
+        filter: 'brightness(0.8)',
       }}
       className="h-[400px] relative"
     >
       <div className="absolute flex flex-col items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="w-155 h-23 text-white text-base font-normal tracking-wide">
+        <div className="w-155 h-23 text-white text-base font-normal tracking-wide font-condensed">
           LANDA HOLDING
         </div>
-        <div className="w-373 h-12 text-white text-4xl font-normal tracking-wider">
+        <div className="w-373 h-12 text-white text-4xl font-normal tracking-wider font-gilda">
           Promotional Video
         </div>
-        <button className="relative w-14 h-14 bg-zinc-300 rounded-full border border-zinc-300 border-spacing-2">
+        <button className="relative w-14 h-14 bg-zinc-300 rounded-full border border-primary border-spacing-2">
           <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <svg
-              className="w-6 h-6 text-stone-500"
+              className="w-6 h-6 text-primary"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
--- components/UpcomingEvents.tsx
+++ components/UpcomingEvents.tsx
@@ -2,90 +2,97 @@ import React from 'react';
 
 export default function UpcomingEvents() {
   return (
+    // TODO: Ask from ariana about ui in mobile.
     <div className="h-[480px] relative bg-stone-100">
-      <div className="left-[185px] top-[29px] absolute text-neutral-800 text-[40px] font-normal">
+      <div className="left-[185px] top-[29px] absolute text-neutral-800 text-4xl font-normal font-gilda">
         UPCOMING EVENT
       </div>
-      <div className="pr-[53px] left-[174px] top-[118px] absolute flex-col justify-center items-start gap-[18px] inline-flex">
+      <div className="pr-14 left-[174px] top-[118px] absolute flex-col justify-center items-start gap-5 inline-flex">
         <div className="self-stretch items-center gap-[625px] inline-flex justify-between">
-          <div className="justify-start items-center gap-[69px] flex">
-            <div className="px-8 flex-col justify-start items-center inline-flex">
-              <div className="text-right text-[#AA8453] text-[40px] font-medium">
+          <div className="justify-start items-center gap-16 flex">
+            <div className="px-8 flex-col justify-start items-center inline-flex font-condensed">
+              <div className="text-right text-primary text-4xl font-medium">
                 08
               </div>
-              <div className="text-right text-[#AA8453] text-3xl font-medium">
+              <div className="text-right text-primary text-3xl font-medium">
                 oct
               </div>
             </div>
-            <div className="flex-col justify-start items-start gap-2 inline-flex">
+            <div className="flex-col justify-start items-start gap-2 inline-flex font-condensed">
               <div className="text-right text-neutral-800 text-3xl font-medium">
                 Investment
               </div>
-              <div className="text-neutral-400 text-base font-normal tracking-widest">
+              <div className="text-neutral-400 text-base font-normal tracking-widest font-condensed">
                 MONDAY/ 2022
               </div>
             </div>
           </div>
-          <div className="h-[42px] px-[50px] py-1 bg-[#AA8453] flex-col justify-between items-center inline-flex">
-            <div className="text-neutral-50 text-2xl font-medium">REGISTER</div>
+          <div className="h-11 px-14 py-1 bg-primary flex-col justify-between items-center inline-flex">
+            <div className="text-neutral-50 text-2xl font-medium font-condensed">
+              REGISTER
+            </div>
           </div>
         </div>
         <div className="self-stretch justify-start items-center gap-[650px] inline-flex">
-          <div className="w-[307px] justify-start items-start gap-[69px] flex">
-            <div className="px-8 flex-col justify-start items-center inline-flex">
-              <div className="text-right text-[#AA8453] text-[40px] font-medium">
+          <div className="w-[307px] justify-start items-start gap-16 flex">
+            <div className="px-8 flex-col justify-start items-center inline-flex font-condensed">
+              <div className="text-right text-primary text-4xl font-medium">
                 08
               </div>
-              <div className="text-right text-[#AA8453] text-3xl font-medium">
+              <div className="text-right text-primary text-3xl font-medium">
                 oct
               </div>
             </div>
             <div className="flex-col justify-start items-start gap-2 inline-flex">
-              <div className="text-right text-neutral-800 text-3xl font-medium">
+              <div className="text-right text-neutral-800 text-3xl font-medium font-condensed">
                 Entrepreneur
               </div>
-              <div className="text-neutral-400 text-base font-normal tracking-widest">
+              <div className="text-neutral-400 text-base font-normal tracking-widest font-condensed">
                 MONDAY/ 2022
               </div>
             </div>
           </div>
-          <div className="h-[42px] px-[50px] py-1 bg-[#AA8453] flex-col justify-between items-center inline-flex">
-            <div className="text-neutral-50 text-2xl font-medium">REGISTER</div>
+          <div className="h-11 px-12 py-1 bg-primary flex-col justify-between items-center inline-flex">
+            <div className="text-neutral-50 text-2xl font-medium font-condensed">
+              REGISTER
+            </div>
           </div>
         </div>
-        <div className="self-stretch justify-start items-center gap-[628px] inline-flex">
-          <div className="justify-start items-center gap-[69px] flex">
-            <div className="px-8 flex-col justify-start items-center inline-flex">
-              <div className="text-right text-[#AA8453] text-[40px] font-medium">
+        <div className="self-stretch justify-start items-center gap-[672px] inline-flex">
+          <div className="justify-start items-center gap-16 flex">
+            <div className="px-8 flex-col justify-start items-center inline-flex font-condensed">
+              <div className="text-right text-primary text-[40px] font-medium">
                 08
               </div>
-              <div className="text-right text-[#AA8453] text-3xl font-medium">
+              <div className="text-right text-primary text-3xl font-medium">
                 oct
               </div>
             </div>
             <div className="flex-col justify-start items-start gap-2 inline-flex">
-              <div className="text-right text-neutral-800 text-3xl font-medium">
+              <div className="text-right text-neutral-800 text-3xl font-medium font-condensed">
                 Investment
               </div>
-              <div className="text-neutral-400 text-base font-normal tracking-widest">
+              <div className="text-neutral-400 text-base font-normal tracking-widest font-condensed">
                 MONDAY/ 2022
               </div>
             </div>
           </div>
-          <div className="h-[42px] px-[50px] py-1 bg-[#AA8453] flex-col justify-between items-center inline-flex">
-            <div className="text-neutral-50 text-2xl font-medium">REGISTER</div>
+          <div className="h-11 px-14 py-1 bg-primary flex-col justify-between items-center inline-flex">
+            <div className="text-neutral-50 text-2xl font-medium font-condensed">
+              REGISTER
+            </div>
           </div>
         </div>
       </div>
       <div className="w-16 h-[313.88px] left-[121px] top-[52px] absolute">
-        <div className="w-[360px] h-[0px] left-0 top-[360px] absolute origin-top-left -rotate-90 bg-neutral-800 border border-neutral-800"></div>
-        <div className="w-[55px] h-[0px] left-0 top-[103.03px] absolute bg-neutral-800 border border-neutral-800"></div>
-        <div className="w-[55px] h-[0px] left-0 top-[360px] absolute bg-neutral-800 border border-neutral-800"></div>
-        <div className="w-[55px] h-[0px] left-0 top-[0px] absolute bg-neutral-800 border border-neutral-800"></div>
-        <div className="w-[55px] h-[0px] left-0 top-[198.51px] absolute bg-neutral-800 border border-neutral-800"></div>
-        <div className="w-[11px] h-[11px] left-[50px] top-[97px] absolute bg-neutral-800 rounded-full border border-neutral-800" />
-        <div className="w-[11px] h-[11px] left-[53px] top-[194px] absolute bg-neutral-800 rounded-full border border-neutral-800" />
-        <div className="w-[11px] h-[11px] left-[53px] top-[355px] absolute bg-neutral-800 rounded-full border border-neutral-800" />
+        <div className="w-[360px] h-0 left-0 top-[360px] absolute origin-top-left -rotate-90 bg-neutral-800 border border-neutral-800"></div>
+        <div className="w-14 h-0 left-0 top-[103.03px] absolute bg-neutral-800 border border-neutral-800"></div>
+        <div className="w-14 h-0 left-0 top-[360px] absolute bg-neutral-800 border border-neutral-800"></div>
+        <div className="w-14 h-0 left-0 top-0 absolute bg-neutral-800 border border-neutral-800"></div>
+        <div className="w-14 h-0 left-0 top-[198.51px] absolute bg-neutral-800 border border-neutral-800"></div>
+        <div className="w-3 h-3 left-14 top-[97px] absolute bg-neutral-800 rounded-full border border-neutral-800" />
+        <div className="w-3 h-3 left-14 top-[194px] absolute bg-neutral-800 rounded-full border border-neutral-800" />
+        <div className="w-3 h-3 left-14 top-[355px] absolute bg-neutral-800 rounded-full border border-neutral-800" />
       </div>
     </div>
   );
