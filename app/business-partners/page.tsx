170c673ec2066d61c10aa14cabff1e94eb19497d
Meraj Bighamian <merajbighamian@gmail.com>
2023-07-24 12:16:46 +0430

    chore: add bussines partner ship form and change business directory :

---
+++ app/business-partners/page.tsx
@@ -0,0 +1,10 @@
+import BusinessPartnerShipForm from '@/components/BusinessPartnerShipForm'
+import React from 'react'
+export default function page() {
+  return (
+    <div>
+      Hello
+      <BusinessPartnerShipForm/>
+    </div>
+  )
+}
---
+++ components/BusinessPartnerShipForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function BusinessPartnerShiphtmlform() {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+      </form>
+    </div>
+  );
+}
