71e5d764b8218635549b597efa36b84967070578
SamanKchn <samankarchegani@gmail.com>
2023-07-24 17:10:25 +0330

    chore: added contact-us page

--- components/contact-us/ContactUsForm.tsx
+++ components/contact-us/ContactUsForm.tsx
@@ -1,5 +1,6 @@
 'use client';
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 interface FormData {
   name: string;
@@ -8,7 +9,6 @@ interface FormData {
   subject: string;
   message: string;
 }
-console.log('form running');
 
 export default function ContactUsForm() {
   const initialFormData: FormData = {
@@ -19,71 +19,152 @@ export default function ContactUsForm() {
     message: '',
   };
 
-  const [formData, setFormData] = useState<FormData>(initialFormData);
-  const handleChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>({
+    mode: 'onBlur',
+    defaultValues: initialFormData,
+  });
 
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    console.log(formData);
-    setFormData(initialFormData);
-  }
+  const onSubmit = async (formData: FormData) => {
+    try {
+      const response = await fetch('/api/contact-us', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      reset(initialFormData); // Reset the form after successful submission
+      console.log('Form data sent successfully!');
+    } catch (error) {
+      console.error('Error sending form data:', error);
+    }
+  };
 
   return (
     <div>
       <h2 className="text-5xl font-light text-center">Get in touch</h2>
-      <div className="grid grid-cols-1 gap-6 my-6 md:grid-cols-2">
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          placeholder="Your Name*"
-          className="w-full input input-bordered drop-shadow-lg"
-        />
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="Your Email*"
-          className="w-full input input-bordered drop-shadow-lg"
-        />
-        <input
-          type="text"
-          name="number"
-          value={formData.number}
-          onChange={handleChange}
-          placeholder="Your Number*"
-          className="w-full input input-bordered drop-shadow-lg"
-        />
-        <input
-          type="text"
-          name="subject"
-          value={formData.subject}
-          onChange={handleChange}
-          placeholder="Subject*"
-          className="w-full input input-bordered drop-shadow-lg"
-        />
-        <textarea
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          rows={4}
-          cols={20}
-          className="w-full col-span-1 textarea textarea-bordered md:col-span-2 drop-shadow-lg"
-          placeholder="Message*"
-        ></textarea>
-      </div>
-      <div className="text-center">
-        <form className="mt-3 btn btn-wide btn-neutral" onClick={handleSubmit}>
-          Send Message
-        </form>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid grid-cols-1 gap-6 my-6 md:grid-cols-2">
+          <div className="flex flex-col">
+            <input
+              id="name"
+              type="text"
+              {...register('name', {
+                required: 'Your Name is required.',
+                pattern: {
+                  value: /^[a-z ,.'-]+$/i,
+                  message: 'Enter a valid Name.',
+                },
+              })}
+              placeholder="Your Name*"
+              className={`w-full input input-bordered drop-shadow-lg ${
+                errors.name ? 'border-red-500' : ''
+              }`}
+            />
+            {errors.name && (
+              <p className="text-sm text-yellow-500 mt-2">
+                {errors.name.message}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <input
+              id="email"
+              type="email"
+              {...register('email', {
+                required: 'Your Email is required.',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Enter a valid email address.',
+                },
+              })}
+              placeholder="Your Email*"
+              className={`w-full input input-bordered drop-shadow-lg ${
+                errors.email ? 'border-red-500' : ''
+              }`}
+            />
+            {errors.email && (
+              <span className="text-sm text-yellow-500 mt-2">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <input
+              id="number"
+              type="number"
+              {...register('number', {
+                required: 'Your Number is required.',
+                pattern: {
+                  value: /^\d{11}$/,
+                  message: 'Enter a valid number.',
+                },
+              })}
+              placeholder="Your Number*"
+              className={`w-full input input-bordered drop-shadow-lg ${
+                errors.number ? 'border-red-500' : ''
+              }`}
+            />
+            {errors.number && (
+              <span className="text-sm text-yellow-500 mt-2">
+                {errors.number.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <input
+              id="subject"
+              type="text"
+              {...register('subject', {
+                required: 'Your Subject is required.',
+                pattern: {
+                  value: /^[a-z ,.'-]+$/i,
+                  message: 'Enter a valid Subject.',
+                },
+              })}
+              placeholder="Your Subject*"
+              className={`w-full input input-bordered drop-shadow-lg ${
+                errors.subject ? 'border-red-500' : ''
+              }`}
+            />
+            {errors.subject && (
+              <span className="text-sm text-yellow-500 mt-2">
+                {errors.subject.message}
+              </span>
+            )}
+          </div>
+          <textarea
+            id="message"
+            {...register('message', { required: 'Message is required.' })}
+            rows={4}
+            cols={20}
+            className={`w-full col-span-1 textarea textarea-bordered md:col-span-2 drop-shadow-lg ${
+              errors.message ? 'border-red-500' : ''
+            }`}
+            placeholder="Message*"
+          ></textarea>
+          {errors.message && (
+            <span className="text-sm text-yellow-500 mt-2">
+              {errors.message.message}
+            </span>
+          )}
+        </div>
+        <div className="text-center">
+          <button type="submit" className="mt-3 btn btn-wide btn-neutral">
+            Send Message
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
