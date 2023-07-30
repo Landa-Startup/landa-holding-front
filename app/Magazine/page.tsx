ad18f2de385fce1ceade3f2bab6d6102b69fdc2c
UNHOT <Serverings@protonmail.com>
2023-07-30 12:00:52 +0330

    feat: init

--- app/Magazine/page.tsx
+++ app/Magazine/page.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import LandaMagazine from '@/components/LandaMagazine'
-import LandaMagazineCard from '@/components/LandaMagazineCard'
-import MagazineStartUps from '@/components/MagazineStartUps'
-import Hero from '@/components/Hero'
-import MagazineModernPost from '@/components/MagazineModernPost'
-import Footer from '@/components/Footer'
-
+import React from 'react';
+import LandaMagazine from '@/components/LandaMagazine';
+import LandaMagazineCard from '@/components/LandaMagazineCard';
+import MagazineStartUps from '@/components/MagazineStartUps';
+import Hero from '@/components/Hero';
+import MagazineModernPost from '@/components/MagazineModernPost';
+import Footer from '@/components/Footer';
+import Banner from '@/components/Banner';
 
 export default function page() {
   return (
-  <div>
-    <Hero/>
-    <div className='grid grid-row w-screen'>
-      <div className='grid grid-cols-5 divide-x-2 divide-neutral-400 h-[884px] bg-neutral-500'>
-        <div className='col-span-2 bg-neutral-100'><MagazineStartUps/></div>
-        <div className='col-span-3 bg-neutral-100'>
-          <MagazineModernPost/>
-        </div>  
-
+    <div>
+      <Banner
+        image="/static/images/b56efbfe712a222c7f569520462f2703.jpg"
+        title="Magazine"
+      />
+      <div className="grid grid-row w-screen">
+        <div className="grid grid-cols-5 divide-x-2 divide-neutral-400 h-[884px] bg-neutral-500">
+          <div className="col-span-2 bg-neutral-100">
+            <MagazineStartUps />
+          </div>
+          <div className="col-span-3 bg-neutral-100">
+            <MagazineModernPost />
+          </div>
+        </div>
       </div>
-
-
     </div>
-  
-  </div>
-  )
+  );
 }
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-export default function Banner() {
+export default function Banner({
+  image,
+  title,
+}: {
+  image: string;
+  title: string;
+}) {
   return (
-    <div>Banner</div>
-  )
+    <div
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        
+      }}
+      className="h-[512px] relative"
+    >
+      <div className="w-[323px] h-[71px] flex-col justify-start items-start absolute top-64 left-32 inline-flex">
+        <div className="text-neutral-50 text-base font-normal tracking-[5.60px] font-condensed">
+          LANDA HOLDING
+        </div>
+        <div className="text-neutral-50 text-[64px] font-normal font-gilda">
+          {title}
+        </div>
+      </div>
+    </div>
+  );
 }
