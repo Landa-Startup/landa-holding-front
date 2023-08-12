93bacac5f05f6fb469f75fc860a6e2fb3d1cd395
Sajjad Momeni <sajjad5221@gmail.com>
2023-08-12 17:40:23 +0330

    fix: fixed confilicts

--- app/investor-registration/page.tsx
+++ app/investor-registration/page.tsx
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-// import Banner from '@/components/Banner'
-=======
-import Banner from '@/components/molecules/Banner'
->>>>>>> origin/develop
 import InvestorRegistrationForm from '@/components/organisms/InvestorRegistrationForm'
 import React from 'react'
 
 export default function page() {
   return (
     <>
-          {/* <Banner
-        image="/static/images/Work-with-us/fb8f5583aaf3e9e272e717954c84f0be.png"
-        title="INVESTOR REGISTRATION"
-      /> */}
-      <InvestorRegistrationForm/>
+      <InvestorRegistrationForm />
     </>
   )
 }
