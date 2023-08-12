1a595ce3c276aae50f447f903ff4bd870a33151e
SamanKchn <samankarchegani@gmail.com>
2023-08-12 21:08:29 +0330

    fix:fix some responsive problems

--- components/organisms/InvestorRegistrationForm.tsx
+++ components/organisms/InvestorRegistrationForm.tsx
@@ -52,10 +52,10 @@ export default function InvestorRegistrationForm() {
               type="text"
               label="First Name"
               required="First Name is Required."
-              patternValue="^[A-Za-z]+$"
-              patternMessage="Only Alphabetic Characters are Allowed."
+              patternValue=""
+              patternMessage=""
               placeholder="Enter your First Name"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
+              className="col-span-1 w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
               labelClass="text-[#6b6b6b] dark:text-current"
             />
 
@@ -66,10 +66,10 @@ export default function InvestorRegistrationForm() {
               type="text"
               label="Last Name"
               required="Last Name is Required."
-              patternValue="^[A-Za-z]+$"
-              patternMessage="Only Alphabetic Characters are Allowed."
+              patternValue=""
+              patternMessage=""
               placeholder="Enter your Last Name"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
+              className="col-span-1 w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
               labelClass="text-[#6b6b6b] dark:text-current"
             />
 
@@ -83,7 +83,21 @@ export default function InvestorRegistrationForm() {
               patternValue="(?:\d{1,2}[-/\s]\d{1,2}[-/\s]'?\d{2,4})|(?:\d{2,4}[-/\s]\d{1,2}[-/\s]\d{1,2})|(?:(?:January|February|March|April|May|June|July|August|September|October|November|December|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Sep|Oct|Nov|Dec)[\s-/,]*?\d{1,2}(?:\s)*(?:rd|th|st)?(?:\s)*[-/,]?(?:\s)*'?\d{2,4})|(?:\d{1,2}(?:\s)*(?:rd|th|st)?(?:\s)*(?:January|February|March|April|May|June|July|August|September|October|November|December|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Sep|Oct|Nov|Dec)(?:\s)*?[-/,]?(?:\s)*'?\d{2,4})"
               patternMessage="Please enter a valid Birth Date (e.g., 2001/02/11)"
               placeholder="Enter your Birth Date"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
+              className="col-span-1 w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
+              labelClass="text-[#6b6b6b] dark:text-current"
+            />
+
+            <Input
+              register={register}
+              errors={errors}
+              nameInput="email"
+              type="email"
+              label="Email Address"
+              required="Email Address is Required."
+              patternValue="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+              patternMessage="Enter a Valid Email Address"
+              placeholder="Enter your Email Address"
+              className="col-span-1 w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
               labelClass="text-[#6b6b6b] dark:text-current"
             />
 
@@ -94,10 +108,10 @@ export default function InvestorRegistrationForm() {
               type="text"
               label="Country of Residence"
               required="Country of Residence is Required."
-              patternValue="^[A-Za-z]+$"
-              patternMessage="Only Alphabetic Characters are Allowed."
+              patternValue=""
+              patternMessage=""
               placeholder="Enter your Country of Residence"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
+              className="col-span-1 w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
               labelClass="text-[#6b6b6b] dark:text-current"
             />
 
@@ -108,53 +122,11 @@ export default function InvestorRegistrationForm() {
               type="text"
               label="Province of Residence"
               required="Province of Residence is Required."
-              patternValue="^[A-Za-z]+$"
-              patternMessage="Only Alphabetic Characters are Allowed."
+              patternValue=""
+              patternMessage=""
               placeholder="Enter your Province of Residence"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
+              className="col-span-1 w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
               labelClass="text-[#6b6b6b] dark:text-current"
-            />
-
-            <Input
-              register={register}
-              errors={errors}
-              nameInput="streetAddress"
-              type="text"
-              label="Street Address"
-              required="Street Address is Required."
-              placeholder="Enter your Street Address"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
-              labelClass="text-[#6b6b6b] dark:text-current"
-              patternValue={''}
-              patternMessage={''}
-            />
-
-            <Input
-              register={register}
-              errors={errors}
-              nameInput="streetAddressLine2"
-              type="text"
-              label="Street Address Line 2"
-              placeholder="Enter Additional Address Details"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
-              labelClass="text-[#6b6b6b] dark:text-current"
-              required={''}
-              patternValue={''}
-              patternMessage={''}
-            />
-
-            <Input
-              register={register}
-              errors={errors}
-              nameInput="postalCode"
-              type="text"
-              label="Postal/Zip Code"
-              required="Postal/Zip Code is Required."
-              placeholder="Enter your Postal or Zip Code"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
-              labelClass="text-[#6b6b6b] dark:text-current"
-              patternValue={''}
-              patternMessage={''}
             />
 
             <Input
@@ -165,10 +137,10 @@ export default function InvestorRegistrationForm() {
               label="Company Name"
               required="Company Name is Required."
               placeholder="Enter your Company Name"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
+              className="col-span-1 w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
               labelClass="text-[#6b6b6b] dark:text-current"
-              patternValue="^[A-Za-z]+$"
-              patternMessage="Only Alphabetic Characters are Allowed."
+              patternValue=""
+              patternMessage=""
             />
 
             <Input
@@ -179,51 +151,35 @@ export default function InvestorRegistrationForm() {
               label="Investment Ceiling"
               required="Investment Ceiling is Required."
               placeholder="Enter your Investment Ceiling"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
+              className="col-span-1 w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
               labelClass="text-[#6b6b6b] dark:text-current"
               patternValue={''}
               patternMessage={''}
             />
 
-            <Input
+            <Select
               register={register}
               errors={errors}
-              nameInput="positionInTeam"
-              type="text"
-              label="Your Position in Team"
-              required="Position in Team is Required."
-              placeholder="Enter your Position in Team"
-              className="w-full mt-3 mb-1 input input-bordered drop-shadow-lg placeholder-[#b2b1b0] dark:placeholder-[#9CA3AF]"
+              nameInput="preferredAreas"
+              label="Preferred Areas for Investment"
+              placeholder="Select your Preferred Area"
+              required="Preferred Areas is Required."
+              options={test}
+              className=" col-start-2 col-span-3 w-full mt-3 mb-1 select select-bordered drop-shadow-lg text-[#b2b1b0] dark:text-[#9CA3AF] "
               labelClass="text-[#6b6b6b] dark:text-current"
-              patternValue={''}
-              patternMessage={''}
             />
-            <div className="flex flex-col col-span-3 mt-3">
-              <Select
-                register={register}
-                errors={errors}
-                nameInput="preferredAreas"
-                label="Preferred Areas for Investment"
-                placeholder="Select your Position in Team"
-                required="Preferred Areas is Required."
-                options={test}
-                className=" w-full mt-3 mb-1 select select-bordered drop-shadow-lg text-[#b2b1b0] dark:text-[#9CA3AF]"
-                labelClass="text-[#6b6b6b] dark:text-current"
-              />
-            </div>
-            <div className="flex flex-col col-span-3">
-              <Select
-                register={register}
-                errors={errors}
-                nameInput="howDidYouKnowUs"
-                label="How did You Get to Know Us?"
-                placeholder="Select How did You Get to Know Us?"
-                required="This field is Required."
-                options={test}
-                className=" w-full mt-3 mb-1 select select-bordered drop-shadow-lg text-[#b2b1b0] dark:text-[#9CA3AF]"
-                labelClass="text-[#6b6b6b] dark:text-current"
-              />
-            </div>
+
+            <Select
+              register={register}
+              errors={errors}
+              nameInput="howDidYouKnowUs"
+              label="How did You Get to Know Us?"
+              placeholder="Select How did You Get to Know Us?"
+              required="This field is Required."
+              options={test}
+              className="col-start-2 col-span-3 w-full mt-3 mb-1 select select-bordered drop-shadow-lg text-[#b2b1b0] dark:text-[#9CA3AF] "
+              labelClass="text-[#6b6b6b] dark:text-current"
+            />
           </div>
           <div className="text-center">
             <button
