92a2d062c88ab09989b52b454256718e56a50893
Mahrokh Lotfi <mahrokhlf00@gmail.com>
2023-08-20 12:04:26 +0330

    fix: about us cards

--- components/molecules/AboutUscard.tsx
+++ components/molecules/AboutUscard.tsx
@@ -12,37 +12,36 @@ const AboutUsCart = ({ title, text, image, reverse, description}: AboutUsCardPro
   return (
     <>
       {reverse && (
-        <div className='flex flex-col-reverse mt-32 text-justify md:grid md:grid-cols-2 md:mb-20'>
-          <div className='font-barlow text-black md:text-lg	 leading-6 mt-[120px] items-center mx-8 not-italic md:-mt-28 md:w-[587px] md:h-[187px] '>
-            <p className='text-black font-gilda text-2xl not-italic font-normal  items-center leading-6 text-start pr-[75px] md:mt-20 '>{title}</p>
-            <p>{text}</p>
-            <button className=' md:text-center md:justify-center bg-[#AA8453] text-white mt-10	md:ml-40 w-[180px]  flex px-[61px] py-[14px] justify-center	gap-10 shrink-0 font-barlow  ml-16 mb-10'>Visit Now</button>
+        <div className='flex flex-col-reverse py-12 leading-6 justify-items-center md:grid md:grid-cols-2  '>
+          <div className='px-[30px] md:px-0 leading-6 text-black md:ml-8 '>
+            <p className='font-gilda text-3xl md:pr-8 text-center mt-3'>{title}</p>
+            <p className='font-barlow  text-normal leading-6 justify-left mt-5 '>{text}</p>
+            <button className=' justify-items-center mx-auto bg-[#AA8453] text-white md:mt-20 mt-8  flex px-10 py-2 font-barlow'>Visit Now</button>
           </div>
-          <div className='grid grid-cols-1 md:pl-20 -mt-28 '>
-            {/* <img className="md:w-[589px] md:h-[357px] flex-shrink-0 -mb-[90px] w-[430px] h-[110px] md:mt-20 " style={{
+          <div className='col-span-1 md:pl-16'>
+            <img className="mb-2 md:w-[650px] md:h-[300px] w-[430px] h-[110px] md:mt-2 " style={{
               backgroundImage: `url(${image})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center'
-            }} /> */}
-
+            }} />
           </div>
         </div>
       )}
       {!reverse && (
-        <div className='flex flex-col md:grid md:grid-cols-2 md:mb-20'>
-          <div className='grid grid-cols-1 md:mb-36'>
-            {/* <img className="md:w-[589px] md:h-[357px] md:mt-48 w-[430px] h-[110px] flex-shrink: md:mr-[200px] -mb-20  " src={image} alt="Aboutuscart Image" style={{
+        <div className='flex md:py-12 flex-col md:grid md:grid-cols-2'>
+          <div className='col-span-1'>
+          <img className="mb-2 md:w-[650px] md:h-[300px] w-[430px] h-[150px] md:mt-2 " style={{
               backgroundImage: `url(${image})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center'
-            }} /> */}
+            }} />
           </div>
-          <div className='font-barlow leading-6 mt-[120px]  not-italic text-black md:text-lg	px-[30px]'>
-            <p className='mb-2 text-2xl not-italic font-normal leading-6 text-black md:mt-20 font-gilda text-start'>{title}</p>
-            <p >{text}</p>
-            <button className='md:text-center md:ml-48 md:justify-center	md:items-center	md:gap-2.5 	bg-[#AA8453]  text-white mt-10	 w-[180px]  flex px-[61px] py-[14px] justify-center	gap-10 shrink-0  font-barlow -mb-20 ml-8 '>Visit Now</button>
+          <div className='col-span-1 font-barlow leading-6 mt-1 text-black	px-[30px]'>
+            <p className='text-3xl font-normal text-black mt-3 text-center font-gilda'>{title}</p>
+            <p className='mt-5 text-normal' >{text}</p>
+            <button className=' justify-items-center mx-auto bg-[#AA8453] text-white md:mt-24 mt-8  flex px-10 py-2 font-barlow'>Visit Now</button>
           </div>
-          <p className="text-black font-Gilda Display text-sm	 not-italic	font-normal	leading-normal  ml-[110px]	 ">
+          <p className="text-black font-Gilda Display text-sm	font-normal	leading-normal  ml-[110px]">
             {description}
           </p>
         </div>
