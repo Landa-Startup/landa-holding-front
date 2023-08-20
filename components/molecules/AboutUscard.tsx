a17b77f6c2bb39c699a431ab15109dae823c0df5
Mahrokh Lotfi <mahrokhlf00@gmail.com>
2023-08-20 12:12:08 +0330

    chore: changing button's margin

--- components/molecules/AboutUscard.tsx
+++ components/molecules/AboutUscard.tsx
@@ -16,10 +16,10 @@ const AboutUsCart = ({ title, text, image, reverse, description}: AboutUsCardPro
           <div className='px-[30px] md:px-0 leading-6 text-black md:ml-8 '>
             <p className='font-gilda text-3xl md:pr-8 text-center mt-3'>{title}</p>
             <p className='font-barlow  text-normal leading-6 justify-left mt-5 '>{text}</p>
-            <button className=' justify-items-center mx-auto bg-[#AA8453] text-white md:mt-20 mt-8  flex px-10 py-2 font-barlow'>Visit Now</button>
+            <button className=' justify-items-center mx-auto bg-[#AA8453] text-white md:mt-24 mt-8  flex px-10 py-2 font-barlow'>Visit Now</button>
           </div>
           <div className='col-span-1 md:pl-16'>
-            <img className="mb-2 md:w-[650px] md:h-[300px] w-[430px] h-[110px] md:mt-2 " style={{
+            <img className="mb-2 md:w-[650px] md:h-[320px] w-[430px] h-[110px] md:mt-2 " style={{
               backgroundImage: `url(${image})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center'
@@ -30,7 +30,7 @@ const AboutUsCart = ({ title, text, image, reverse, description}: AboutUsCardPro
       {!reverse && (
         <div className='flex md:py-12 flex-col md:grid md:grid-cols-2'>
           <div className='col-span-1'>
-          <img className="mb-2 md:w-[650px] md:h-[300px] w-[430px] h-[150px] md:mt-2 " style={{
+          <img className="mb-2 md:w-[589px] md:h-[357px] w-[430px] h-[150px] md:mt-2 " style={{
               backgroundImage: `url(${image})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center'
@@ -39,7 +39,7 @@ const AboutUsCart = ({ title, text, image, reverse, description}: AboutUsCardPro
           <div className='col-span-1 font-barlow leading-6 mt-1 text-black	px-[30px]'>
             <p className='text-3xl font-normal text-black mt-3 text-center font-gilda'>{title}</p>
             <p className='mt-5 text-normal' >{text}</p>
-            <button className=' justify-items-center mx-auto bg-[#AA8453] text-white md:mt-24 mt-8  flex px-10 py-2 font-barlow'>Visit Now</button>
+            <button className=' justify-items-center mx-auto bg-[#AA8453] text-white md:mt-28 mt-8  flex px-10 py-2 font-barlow'>Visit Now</button>
           </div>
           <p className="text-black font-Gilda Display text-sm	font-normal	leading-normal  ml-[110px]">
             {description}
