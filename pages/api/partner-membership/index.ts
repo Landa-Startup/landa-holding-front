fb2dfddbab7c336d89dbcf6a675dac64fcf47805
Mahrokh Lotfi <mahrokhlf00@gmail.com>
2023-08-20 13:11:27 +0330

    chore: fixing investor and partner form data

--- pages/api/partner-membership/index.ts
+++ pages/api/partner-membership/index.ts
@@ -20,7 +20,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
       const formData: FormData = req.body;
       console.log('Received form data:', formData);
-      const savedFormData = await prisma.partnerMembership.create({ data: formData });
+      const savedFormData = await prisma.partnerMembership.create({ data:{
+        birthDate :formData.birthDate,
+        firstName : formData.firstName,
+        lastName : formData.lastName,
+        email: formData.email,
+        countryOfResidence:formData.countryOfResidence,
+        provinceOfResidence : formData.provinceOfResidence,
+        companyName : formData.companyName,
+        investmentCeiling : formData.investmentCeiling,
+        howDidYouKnowUs : formData.howDidYouKnowUs,
+
+      } });
       console.log('Saved form data:', savedFormData);
       res.status(200).json({ message: 'Form data received and processed successfully.' });
     } catch (error) {
