98d45f64caf1b94c9e2cb55488a72ebb8201e585
SamanKchn <samankarchegani@gmail.com>
2023-08-21 16:49:32 +0330

    feat: added Seo Course Page

---
+++ components/atoms/IconYellow.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function IconYellow() {
+  return (
+    <div className="self-center">
+      <svg
+        width="34"
+        height="34"
+        viewBox="0 0 34 34"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="17" cy="17" r="17" fill="#FDD30A" />
+      </svg>
+    </div>
+  );
+}
