1b871db6278400f081eefd3c8dfedd3726fbd1fb
UNHOT <Serverings@protonmail.com>
2023-08-27 13:22:12 +0330

    chore: Fix backgrounds.

--- components/molecules/AboutUsProfile.tsx
+++ components/molecules/AboutUsProfile.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import AboutUsProfileCard from './AboutUsProfileCard';
 
 export default function AboutUsProfile() {
@@ -16,25 +16,32 @@ export default function AboutUsProfile() {
     {
       image: '/static/images/About/4-1 1 (1).png',
       title: 'Hadi Hasanpour',
-      description: 'CO-FOUNDER & INVESTOR'
+      description: 'CO-FOUNDER & INVESTOR',
     },
   ];
 
-
-
   return (
-    <div className=''>
-      <div className='flex flex-col m-8 text-black justify-items-center md:ps-24 md:ms-28'>
-        <p className=' font-normal leading-normal tracking-widest font-Barlow-Condensed md:text-xl '> PROFESSIONALS</p>
-        <p className='text-3xl font-normal leading-normal font-gilda md:text-4xl '>Managing Directors</p>
-      </div >
+    <div className="bg-[#FAFAFA]">
+      <div className="flex flex-col mx-8 py-5 text-black justify-items-center md:ps-24 md:ms-28">
+        <p className=" font-normal leading-normal tracking-widest font-Barlow-Condensed md:text-xl ">
+          {' '}
+          PROFESSIONALS
+        </p>
+        <p className="text-3xl font-normal leading-normal font-gilda md:text-4xl ">
+          Managing Directors
+        </p>
+      </div>
 
-      <div className="flex flex-col m-8 md:grid md:grid-cols-3 md:gap-8 md:mx-20 md:px-32 ">
+      <div className="flex flex-col mx-8 md:grid md:grid-cols-3 md:gap-8 md:mx-20 md:px-32">
         {cardsData.map((card, index) => (
-          <AboutUsProfileCard key={card.title} image={card.image} title={card.title} description={card.description} />
+          <AboutUsProfileCard
+            key={card.title}
+            image={card.image}
+            title={card.title}
+            description={card.description}
+          />
         ))}
       </div>
     </div>
-
   );
 }
