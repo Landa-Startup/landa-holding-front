012587373bdf0a9b2feb5ebb799af9483f563a83
Sajjad Momeni <sajjad5221@gmail.com>
2023-08-31 13:20:50 +0330

    fix: fix our-partners images

--- components/templates/ContactUs.tsx
+++ components/templates/ContactUs.tsx
@@ -7,19 +7,18 @@ export default function ContactUs() {
   // TODO: use daisyUI for styling
   return (
     <div className="md:h-[644px] bg-neutral-50 flex flex-col md:flex-row justify-between items-center md:px-36">
-      {/* TODO: show images with javascript map */}
       <div className="md:w-[515px] md:h-[476px] flex flex-col items-center justify-center px-12 py-10">
         <p className="text-justify">
           At Landa Holding, we welcome strategic affiliations with international firms and qualified influencers alike. As an approved affiliate, you will receive co-branded support and future representations that wield substantial influence within the business landscape.
           To become an affiliate, please complete our free questionnaire for our confidential review.
         </p>
         <Link href={'/partner-membership'} >
-          <Button text="Join Us" size='' />
+          <Button text="Join Us" size='visit' />
         </Link>
       </div>
       <div className="flex gap-20 md:h-full relative mt-5 md:my-0 md:mb-0">
-        <div className="absolute top-0 w-full h-16 md:h-48 bg-gradient-to-b from-white via-transparent-white z-10" />
-        <div className="absolute bottom-0 w-full h-16 md:h-60 bg-gradient-to-t from-white via-transparent-white z-10" />
+        <div className="absolute top-0 w-full h-16 md:h-48  z-10" />
+        <div className="absolute bottom-0 w-full h-16 md:h-60  via-transparent-white z-10" />
         <div className="flex flex-col justify-around">
           <div className="spin-image">
             <Image
@@ -71,7 +70,7 @@ export default function ContactUs() {
           <div className="spin-image-top">
             <Image
               className=""
-              src="/static/images/Home/universities/6.jpg"
+              src="/static/images/Home/universities/6.png"
               alt="t"
               width={100}
               height={100}
@@ -80,7 +79,7 @@ export default function ContactUs() {
           <div className="spin-image-top">
             <Image
               className=""
-              src="/static/images/Home/universities/7.jpg"
+              src="/static/images/Home/universities/77.png"
               alt="t"
               width={100}
               height={100}
@@ -89,14 +88,14 @@ export default function ContactUs() {
           <div className="spin-image-top">
             <Image
               className=""
-              src="/static/images/Home/universities/8.jpg"
+              src="/static/images/Home/universities/88.png"
               alt="t"
               width={100}
               height={100}
             />
           </div>
         </div>
-        <div className="flex flex-col justify-around">
+        <div className="flex flex-col justify-start md:mt-5">
           <div className="spin-image">
             <Image
               className=""
