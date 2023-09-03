1cbebd005661625a10671ef46b3110030ed92be5
UNHOT <Serverings@protonmail.com>
2023-09-03 13:26:42 +0330

    refactor: Minor fixes.

--- components/atoms/MagazineModernPost.tsx
+++ components/atoms/MagazineModernPost.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import MagazineModernPostCard from './cards/MagazineModernPostCard';
 import MagazineModernPostCard2 from './cards/MagazineModernPostCard2';
 import MagazineModernPostCard3 from './cards/MagazineModernPostCard3';
@@ -8,29 +7,23 @@ import MagazineModernPostBottomSpace from './MagazineModernPostBottomSpace';
 export default function MagazineModernPost() {
   return (
     <div>
-      <div className="mx-auto">
-        <h2 className="divide-y-8 text-slate-950 text-4xl text-left mt-5 ml-20 px-16">
-          MODERN POST
-        </h2>
-      </div>
-      <div className="mx-auto">
-        <h3 className="text-slate-950 text-1xl text-left mt-5 px-36 text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-          commodi iste nobis est voluptatem voluptates ratione provident eos
-          culpa, neque unde ab minima harum! Autem veniam similique laboriosam
-          dolorem mollitia.
-        </h3>
+      <h1 className="text-slate-950 text-4xl text-left mt-5 ml-20 px-16">
+        MODERN POST
+      </h1>
+      <div className="text-slate-950 text-1xl mt-5 px-36 text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        commodi iste nobis est voluptatem voluptates ratione provident eos
+        culpa, neque unde ab minima harum! Autem veniam similique laboriosam
+        dolorem mollitia.
       </div>
       <MagazineModernPostCard />
-      <hr className="h-0.5 mt-4 mb-9 mr-64 bg-gray-100 border-0 dark:bg-gray-700"></hr>
+      <hr className="horizontal-rule" />
       <MagazineModernPostCard2 />
-      <hr className="h-0.5 mt-4 mr-64 mb-9 bg-gray-100 border-0 dark:bg-gray-700"></hr>
+      <hr className="horizontal-rule" />
       <MagazineModernPostCard3 />
-      <hr className="h-0.5 mt-4 mr-64 mb-9 bg-gray-100 border-0 dark:bg-gray-700"></hr>
-      <div className="mx-auto">
-        <h3 className="text-slate-950 text-3xl text-left text-bold mt-5 px-24 mb-6 text-justify">
-          12 DECEMBER 2022
-        </h3>
+      <hr className="horizontal-rule" />
+      <div className="text-slate-950 text-3xl text-bold mt-5 px-24 mb-6 text-justify">
+        12 DECEMBER 2022
       </div>
       <MagazineModernPostBottomSpace />
     </div>
--- components/atoms/MagazineModernPostBottomCard.tsx
+++ components/atoms/MagazineModernPostBottomCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+
 export default function MagazineModernPostBottomCard({
   image,
   description,
@@ -8,22 +9,13 @@ export default function MagazineModernPostBottomCard({
   description: string;
 }) {
   return (
-    <div className='px-4 mb-1'>
+    <div className="px-4 mb-1">
       <div className="flex justify-center">
-        <Image
-          className=""
-          src={'/static/images/Magazine/Date/magazines.png'}
-          alt="Landa"
-          width={330}
-          height={430}
-        />
+        <Image src={image} alt="Landa" width={330} height={430} />
       </div>
       <div className="flex justify-start mt-3 mb-3">
         <p className="px-6 text-justify text-slate-950 text-bold">
-          Quisque pretium fermentum quam, sit amet cursus ante sollicitudin vel.
-          Morbi consequat risus consequat, porttitor orci sit amet, iaculis
-          nisl. Integer quis sapien neceli ultrices euismod sit amet id lacus.
-          Sed a imperdiet erat. Duis eu est dignissim lacus...
+          {description}
         </p>
       </div>
     </div>
