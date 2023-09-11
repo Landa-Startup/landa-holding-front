e238df0ba89d3fab6f244a471c5f9ada36797c22
UNHOT <Serverings@protonmail.com>
2023-09-11 11:33:58 +0330

    chore: Add mobile lines.

--- app/academy/page.tsx
+++ app/academy/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Hero from '@/components/templates/Hero';
 import RoadmapContainer from '@/components/academy/Roadmap/RoadmapContainer';
 import PromoteCourse from '@/components/academy/PromoteCourse';
 import Gallery from '@/components/academy/Gallery';
 import ContactUs from '@/components/academy/ContactUs';
 import ScrollMenu from '@/components/academy/ScrollMenu';
 import { Metadata } from 'next';
+import Hero from '@/components/academy/Hero';
 
 export const metadata: Metadata = {
   title: 'Landa Academy',
@@ -18,7 +18,7 @@ export default function SeoCoursePage() {
       {/* Hero Section */}
       <Hero
         showLanda={false}
-        titles="SEO COURSE"
+        title="SEO COURSE"
         subTitle="Elevate your online impact with our SEO course at Landa Academy! Master search engine optimization, from keywords to cutting-edge strategies. Join us now and amplify your digital presence!"
         buttonBg="#FDD30A"
         backgroundImage="586df33bf776763ec00a0ed6ea0528cb.jpg"
---
+++ components/academy/Hero.tsx
@@ -0,0 +1,90 @@
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import Button from '../atoms/Button';
+
+export default function Hero({
+  showLanda,
+  title,
+  subTitle,
+  buttonBg,
+  backgroundImage,
+  leftImage,
+  showButton,
+}: {
+  showLanda: boolean;
+  title: string;
+  subTitle: string;
+  buttonBg?: string;
+  backgroundImage: string;
+  leftImage: string;
+  showButton?: boolean;
+}) {
+  return (
+    <div
+      style={{
+        backgroundImage: `url('/static/images/Home/Hero/${backgroundImage}')`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }}
+      className="h-screen relative overflow-hidden"
+    >
+      <div className="flex flex-col items-center justify-center md:h-screen space-y-5 py-36">
+        <div
+          className={` ${
+            showLanda ? 'block' : 'hidden'
+          } text-center text-white text-2xl md:tracking-[11.20px] font-condensed font-normal tracking-[7px]`}
+        >
+          Landa Holding
+        </div>
+        <div className="hidden md:flex text-center font-barlow text-neutral-50 text-opacity-75 text-4xl font-semibold leading-10 tracking-[2px] flex-col space-y-2">
+          {title}
+        </div>
+
+        <div className="hidden md:block text-center font-barlow text-neutral-50 text-opacity-95 text-4xl font-semibold leading-10 tracking-[2px]">
+          {subTitle}
+        </div>
+        <div className="md:hidden text-center w-[300px] font-barlow text-neutral-50 text-[20px] md:text-4xl font-normal leading-8 md:leading-10 md:tracking-[2px]">
+          Landa Holding is an international investment company active in Iran
+          and Canada.
+        </div>
+        {showButton ? (
+          <Button
+            text="Register Now"
+            size="notVisit"
+            bgColor={buttonBg}
+            addedClass={buttonBg ? 'md:hidden' : 'block'}
+          />
+        ) : (
+          <></>
+        )}
+        <Image
+          className="w-[365px] md:w-[412px] h-[497px] md:h-[560px] absolute -right-16 md:right-0 bottom-0"
+          src={`/static/images/${leftImage}`}
+          alt="Landa"
+          width={500}
+          height={500}
+        />
+        <Link
+          className="absolute flex items-center justify-center w-10 h-10 md:w-12 md:h-12 border border-white rounded-full animate-bounce left-7 md:left-10 bottom-4"
+          href={'#LandaHolding'}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            className="text-white bi bi-arrow-down"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+            />
+          </svg>
+        </Link>
+      </div>
+    </div>
+  );
+}
