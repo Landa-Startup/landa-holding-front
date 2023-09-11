02940c56a8340d32d32cb80b71670deb169ce36a
Sajjad Momeni <sajjad5221@gmail.com>
2023-09-11 16:05:53 +0330

    fix: fixed build

--- app/[locale]/seo-courses/page.tsx
+++ app/[locale]/seo-courses/page.tsx
@@ -14,7 +14,7 @@ export default function page() {
       <div className="bg-white">
         <Hero
           showLanda={false}
-          titles="SEO COURSE"
+          titles={["SEO COURSE"]}
           subTitle="Upgrade yourself! SEO Strategies for Modern Businesses"
           buttonBg="#FDD30A"
           backgroundImage="586df33bf776763ec00a0ed6ea0528cb.jpg"
