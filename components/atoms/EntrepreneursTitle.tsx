81a1dc9c826a34e8b9710435b27bc3703dc30bda
Sajjad Momeni <sajjad5221@gmail.com>
2023-09-21 12:56:19 +0330

    chore: fix form

--- components/atoms/EntrepreneursTitle.tsx
+++ components/atoms/EntrepreneursTitle.tsx
@@ -4,7 +4,7 @@ export default function EntrepreneursTitle(){
   return(
     <>
     <div className='text-center'>
-<p className='mb-20 font-serif tracking-wide text-7xl'>ENTREPRENEURS FORM</p>
+<p className='mb-20 font-serif tracking-wide text-3xl md:text-7xl'>ENTREPRENEURS FORM</p>
     </div>
     <div>
       <p className='mb-4 text-4xl'>Personal information</p>
