757656c591ee25c011232c97082aa1c46c31b013
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-10 13:47:55 +0330

    Feat: Recreate login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -7,10 +7,6 @@ export default function LoginPage() {
 
   return (
     <div>
-      <Banner
-        image="/static/images/Contact/37b76f28c1c41b4ea18163cf2fba85ab.png"
-        title="Contact Us"
-      />
       <LoginForm />
     </div>
   )
