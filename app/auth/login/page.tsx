5915843c39b9dbd987f5ddb26fdf095e7bd3da84
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-10 13:52:53 +0330

    Chore: Set metadata

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,13 +1,18 @@
 // pages/login.js
-"use client"
-import LoginForm from '@/components/organisms/LoginForm'
-import Banner from '@/components/molecules/Banner';
+import LoginForm from '@/components/organisms/LoginForm';
+import { Metadata } from 'next';
+
+// Define metadata for the page
+export const metadata: Metadata = {
+  title: 'Landa Holding | Login',
+  description:
+    'Welcome to Landa Holding, where innovation meets excellence. Explore our diverse portfolio, discover our commitment to sustainable growth, and join us on a journey towards a brighter future.',
+};
 
 export default function LoginPage() {
-
   return (
     <div>
       <LoginForm />
     </div>
-  )
+  );
 }
