180d233f16e65515c15aec4bfbf2bf467c6a1cdb
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-15 13:52:58 +0330

    Chore: Minor fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['res.cloudinary.com'],
-  }
+    domains: ['res.cloudinary.com', 'panel.landaholding.com'],
+  },
 };
 
 module.exports = nextConfig;
