6178dc727951d76fe95684df1c3b9c9d09be8d54
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-17 10:51:24 +0330

    Feat: Use components

--- components/panel/MainSection.tsx
+++ components/panel/MainSection.tsx
@@ -4,88 +4,12 @@ import { parseCookies } from 'nookies';
 import { DecodedToken } from 'app/types/global';
 import Table from './Table';
 import LeaveForm from './LeaveForm';
+import TableContainer from './TableContainer';
 export default function MainSection() {
   return (
-    // <div className="flex flex-col gap-14 overflow-x-auto mx-auto mt-10">
-    //   <Table
-    //     header="Employees leave permissions"
-    //     tableHead={[
-    //       'No.',
-    //       'Employee Name',
-    //       'Employer Name',
-    //       'Type Of Leave',
-    //       'Date',
-    //       'Time',
-    //       'Status',
-    //     ]}
-    //     tableData={[
-    //       {
-    //         employerName: 'Cy Ganderton',
-    //         employeeName: 'Quality Control Specialist',
-    //         typeOfLeave: 'illness',
-    //         date: '2023/07/12',
-    //         time: '12:30',
-    //         status: 'Approved',
-    //       },
-    //       {
-    //         employerName: 'Cy Ganderton',
-    //         employeeName: 'Quality Control Specialist',
-    //         typeOfLeave: 'illness',
-    //         date: '2023/07/12',
-    //         time: '12:30',
-    //         status: 'Pending',
-    //       },
-    //       {
-    //         employerName: 'Cy Ganderton',
-    //         employeeName: 'Quality Control Specialist',
-    //         typeOfLeave: 'illness',
-    //         date: '2023/07/12',
-    //         time: '12:30',
-    //         status: 'Rejected',
-    //       },
-    //     ]}
-    //   />
-    //   <Table
-    //     header="Employers leave permissions"
-    //     tableHead={[
-    //       'No.',
-    //       'Employee Name',
-    //       'Employer Name',
-    //       'Type Of Leave',
-    //       'Date',
-    //       'Time',
-    //       'Status',
-    //     ]}
-    //     tableData={[
-    //       {
-    //         employerName: 'Cy Ganderton',
-    //         employeeName: 'Quality Control Specialist',
-    //         typeOfLeave: 'illness',
-    //         date: '2023/07/12',
-    //         time: '12:30',
-    //         status: 'Approved',
-    //       },
-    //       {
-    //         employerName: 'Cy Ganderton',
-    //         employeeName: 'Quality Control Specialist',
-    //         typeOfLeave: 'illness',
-    //         date: '2023/07/12',
-    //         time: '12:30',
-    //         status: 'Pending',
-    //       },
-    //       {
-    //         employerName: 'Cy Ganderton',
-    //         employeeName: 'Quality Control Specialist',
-    //         typeOfLeave: 'illness',
-    //         date: '2023/07/12',
-    //         time: '12:30',
-    //         status: 'Rejected',
-    //       },
-    //     ]}
-    //   />
-    // </div>
-    <div>
-      <LeaveForm/>
+    <div className="px-20 py-11">
+      <TableContainer />
+      <LeaveForm />
     </div>
   );
 }
