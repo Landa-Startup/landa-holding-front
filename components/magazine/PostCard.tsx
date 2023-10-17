102cf4de3ffbeb7c2ac930a3adcdb94cca1dd959
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-17 16:03:31 +0330

    Feat: Create magazine pages

--- components/magazine/PostCard.tsx
+++ components/magazine/PostCard.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
 import React from 'react';
 import Button from '../common/Button';
+import Link from 'next/link';
 
 export default function PostCard({
+  index,
   image,
   date,
   title,
   description,
 }: {
+  index: string;
   image: string;
   date: string;
   title: string;
@@ -15,15 +18,31 @@ export default function PostCard({
 }) {
   return (
     <div className="flex flex-col gap-4">
-      <div className="w-[725px] h-[384px] relative">
-        <Image src={image} alt="Landa magazine post" layout="fill" />
-      </div>
+      <Image
+        className="object-cover rounded-sm"
+        src={image}
+        alt="Landa magazine post"
+        width={850}
+        height={384}
+      />
+
       <span className="text-primary font-condensed tracking-[2.4px]">
         {date}
       </span>
-      <span className="font-gilda text-xl tracking-[2.8px]">{title}</span>
+      <Link
+        href={`/magazine/${index}`}
+        className="font-gilda text-xl tracking-[2.8px] hover:text-primary"
+      >
+        {title}
+      </Link>
       <p className="font-barlow text-justify text-[#6B6B6B]">{description}</p>
-      <Button goto="\" text="Read More" size="notVisit"/>
+      <Button
+        goto={`/magazine${index}`}
+        text="Read More"
+        size="notVisit"
+        bgColor="Primary"
+        addedClass="self-start"
+      />
     </div>
   );
 }
