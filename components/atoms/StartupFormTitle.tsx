1ab868593ebc4e7e541a5ed17695595ab69aeb7b
barad <dehghanibarad64@gmail.com>
2023-10-21 15:21:10 +0330

    fix: fixed startupform

--- components/atoms/StartupFormTitle.tsx
+++ components/atoms/StartupFormTitle.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export default function StartupFormTitle() {
-  return (
-    <div className="text-center">
+  return //(
+    /*<div className="text-center">
       <p className="font-serif text-2xl pt-8 mb-20 tracking-wide md:pt-8 md:text-5xl lg:text-6xl xl:text-7xl ">Startup Validation Form</p>
       <p className="font-serif text-lg mb-20 tracking-wide">
         {/* Let&apos;s Talk About Your Project */}
-        Fill us in about Your Project
-      </p>
-    </div>
-  );
-}
+        //Fill us in about Your Project
+      //</p>
+    //</div>
+  //);
+//}
