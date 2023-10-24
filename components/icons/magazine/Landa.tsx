2d57ae1e270c688659e5297c8aaaf4c62b341cb3
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-24 13:22:26 +0330

    Fix: Minor fixes.

--- components/icons/magazine/Landa.tsx
+++ components/icons/magazine/Landa.tsx
@@ -13,14 +13,14 @@ export default function Landa() {
       <path
         d="M350.091 1601L931.909 794.701L917.364 1L582.818 441.945L597.364 782.102L1 1601H350.091Z"
         stroke="black"
-        stroke-opacity="0.5"
-        stroke-width="0.5"
+        strokeOpacity="0.5"
+        strokeWidth="0.5"
       />
       <path
         d="M757.364 1109.66L1106.45 1601H1441L931.909 870.291L757.364 1109.66Z"
         stroke="black"
-        stroke-opacity="0.5"
-        stroke-width="0.5"
+        strokeOpacity="0.5"
+        strokeWidth="0.5"
       />
     </svg>
   );
