774f8bc027348d7f32248099d73cab185df206e0
Sajjad Momeni <sajjad5221@gmail.com>
2023-10-27 00:55:30 +0330

    fix: fixed font in safari

--- components/home/HomeCards.tsx
+++ components/home/HomeCards.tsx
@@ -26,7 +26,7 @@ export default function HomeCards({
 
   return (
     <div
-      className={` ${flexDirectionClass} border-b-2 md:border-none last:border-none border-primary flex justify-between items-center px-9 md:px-32 py-5 md:py-36`}
+      className={` ${flexDirectionClass} border-b-2 md:border-none last:border-none border-primary flex justify-between items-center px-9 md:px-32 py-5 md:py-32`}
     >
       <HomeCardsLeft text={text} addedClass={addedClass} link={link} />
       <HomeCardsRight images={images} titles={titles} link={link} />
