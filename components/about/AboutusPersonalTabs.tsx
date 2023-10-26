7ec8af3cec32f63477466f4f81bca1ef91f08091
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-27 01:25:28 +0330

    Fix: Social logos

--- components/about/AboutusPersonalTabs.tsx
+++ components/about/AboutusPersonalTabs.tsx
@@ -4,6 +4,7 @@ import IconLinkedin from '../icons/IconLinkedin';
 import IconRoundedEmail from '../icons/IconRoundedEmail';
 import IconRoundedInstagram from '../icons/IconInstagramRounded';
 import IconWeb from '../icons/IconWeb';
+import Image from 'next/image';
 
 export default function AboutusPersonalTabs({
   image,
@@ -23,37 +24,36 @@ export default function AboutusPersonalTabs({
   instagram: string;
 }) {
   return (
-    <div
-      className="w-[300px] md:w-[270px] h-[300px] border border-primary rounded-sm flex flex-col  items-center md:ml-12 lg:mt-20"
-      style={{
-        backgroundImage: `url(${image})`,
-        backgroundPosition: 'center',
-      }}
-    >
-      <div className="pl-2 pb-2 mt-1 self-start">
-        <Link
-          href={linkedIn}
-          target="_blank"
-          className=""
-        >
-          <IconLinkedin />
-        </Link>
-      </div>
+    <div className="w-[300px] md:w-[270px] h-[300px] border border-primary rounded-sm flex flex-col md:ml-12 lg:mt-20 relative">
+      <Image
+        className="object-cover"
+        src={image}
+        alt="Landa Directors"
+        layout="fill"
+        quality={90}
+      />
+      <div className="z-10 gap-2 flex flex-col ml-2 mt-2">
+        <div>
+          <Link href={linkedIn} target="_blank">
+            <IconLinkedin />
+          </Link>
+        </div>
 
-      <div className=" pl-2 pb-2 self-start">
-        <a href={email}>
-          <IconRoundedEmail />
-        </a>
-      </div>
-      <div className=" pl-2 pb-2 self-start">
-        <Link href={website} target="_blank">
-          <IconWeb />
-        </Link>
-      </div>
-      <div className=" pl-2 self-start opacity-75  ">
-        <Link href={instagram} target="_blank">
-          <IconRoundedInstagram />
-        </Link>
+        <div>
+          <a href={email}>
+            <IconRoundedEmail />
+          </a>
+        </div>
+        <div>
+          <Link href={website} target="_blank">
+            <IconWeb />
+          </Link>
+        </div>
+        <div>
+          <Link href={instagram} target="_blank">
+            <IconRoundedInstagram />
+          </Link>
+        </div>
       </div>
       <div className="flex flex-col items-center px-4 pb-4 mt-40 lg:mt44  ">
         <span className="font-normal text-lg text-black">{name}</span>
