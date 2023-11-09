eff394ef042cd46a7a271bef6bd7c5f5d8fbfcd1
barad <dehghanibarad64@gmail.com>
2023-11-09 23:17:47 +0330

    fix: fixed partners cars

--- components/home/PartnersStartupCard.tsx
+++ components/home/PartnersStartupCard.tsx
@@ -21,7 +21,7 @@ export default function PartnersStartupCard({
       />
       <div className="flex flex-col gap-2">
         <span className="text-xl">{title}</span>
-        <p className="text-xs leading-5 tracking-[1.3px]">{description}</p>
+        <p className="text-xs leading-4 tracking-[1.3px]">{description}</p>
       </div>
     </div>
   );
