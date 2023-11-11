81003eb3bd15ac22e8afb432bbbad4d2124ac0cc
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-11 15:34:15 +0330

    Chore: Refactor middleware.js to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,40 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { protectedRoutes } from "@/router/routes"
+import { NextResponse } from 'next/server';
+import acceptLanguage from 'accept-language';
+import { fallbackLng, languages, cookieName } from './app/i18n/setting';
 
-export function middleware(request: NextRequest) {
-    const currentUser = request.cookies.get("currentUser")?.value;
-    const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname);
-    const isNotLoggedIn = !currentUser;
-    if (isProtectedRoute && isNotLoggedIn) {
-        return NextResponse.redirect("http://localhost:3000/auth/login");
-    }
-    return NextResponse.next();
+acceptLanguage.languages(languages);
 
+export const config = {
+  // matcher: '/:lng*'
+  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)'],
+};
 
+export function middleware(req: any) {
+  let lng;
+  if (req.cookies.has(cookieName))
+    lng = acceptLanguage.get(req.cookies.get(cookieName).value);
+  if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'));
+  if (!lng) lng = fallbackLng;
+
+  // Redirect if lng in path is not supported
+  if (
+    !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
+    !req.nextUrl.pathname.startsWith('/_next')
+  ) {
+    return NextResponse.redirect(
+      new URL(`/${lng}${req.nextUrl.pathname}`, req.url)
+    );
+  }
+
+  if (req.headers.has('referer')) {
+    const refererUrl = new URL(req.headers.get('referer'));
+    const lngInReferer = languages.find((l) =>
+      refererUrl.pathname.startsWith(`/${l}`)
+    );
+    const response = NextResponse.next();
+    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+    return response;
+  }
+
+  return NextResponse.next();
 }
