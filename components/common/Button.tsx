342a5a6b15eba610f07ba0fbfbbcc4131b93c6c3
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-11 10:01:30 +0330

    Chore: Add Link component to Button and update styles

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React, { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 
@@ -29,26 +30,27 @@ export default function Button({
   const router = useRouter();
 
   return (
-    <button
-      onClick={() => router.push(goto || '/')}
-      className={`btn2 ${
-        isVisitSize
-          ? 'w-[135px] md:w-[219px] h-[32px] md:h-[60px] pl-[72px] pr-[71px] pt-[15px] pb-4 mt-[19px]'
-          : 'hidden md:flex h-11 pl-[72px] pr-[71px] pt-[15px] pb-4 mt-[19px]'
-      } ${
-        bgColor === 'Primary' ? 'bg-primary' : 'bg-[#FDD30A]'
-      } px-10 py-5 relative border border-white uppercase tracking-wider leading-none overflow-hidden inset-0 flex justify-center items-center text-center font-condensed text-white text-base font-normal hover:text-white ${addedClass}`}
-      type={type}
-    >
-      <span className="absolute inset-0 bg-black"></span>
-      <span className="absolute inset-0 flex justify-center items-center text-center font-condensed text-white text-base font-medium leading-none">
-        {text}
-      </span>
-      {isVisitSize && (
+    <Link href={goto}>
+      <button
+        className={`btn2 ${
+          isVisitSize
+            ? 'w-[135px] md:w-[219px] h-[32px] md:h-[60px] pl-[72px] pr-[71px] pt-[15px] pb-4 mt-[19px]'
+            : 'hidden md:flex h-11 pl-[72px] pr-[71px] pt-[15px] pb-4 mt-[19px]'
+        } ${
+          bgColor === 'Primary' ? 'bg-primary' : 'bg-[#FDD30A]'
+        } px-10 py-5 relative border border-white uppercase tracking-wider leading-none overflow-hidden inset-0 flex justify-center items-center text-center font-condensed text-white text-base font-normal hover:text-white ${addedClass}`}
+        type={type}
+      >
+        <span className="absolute inset-0 bg-black"></span>
         <span className="absolute inset-0 flex justify-center items-center text-center font-condensed text-white text-base font-medium leading-none">
           {text}
         </span>
-      )}
-    </button>
+        {isVisitSize && (
+          <span className="absolute inset-0 flex justify-center items-center text-center font-condensed text-white text-base font-medium leading-none">
+            {text}
+          </span>
+        )}
+      </button>
+    </Link>
   );
 }
