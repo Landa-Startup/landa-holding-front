6dc818d8b9ecdb9e41f5ec050354e7fafd7e4d80
Meraj Bighamian <merajbighamian@gmail.com>
2023-11-12 17:12:35 +0330

    chore: add blog details page

---
+++ app/Magazine/[slug]/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { MagazineData } from 'app/types/global';
+import Image from 'next/image';
+import { usePathname, useRouter,useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react';
+ 
+export default function Page() {
+    const pathname = usePathname()
+    const slug = pathname?.replace("/magazine/", "")
+    const [cardData, setCardData] = useState<MagazineData>();
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_DJANGO_HOST_URL}blog/details/${slug}/?format=json`);
+      const data = await response.json();
+      console.log(data)
+      setCardData(data);
+    }
+    fetchTags();
+  }, [slug]);
+
+
+  return (
+    <div>
+        {/* <Image src={cardData?.thumbnail} alt='thumbnail'/> */}
+        <h1>{cardData?.title}</h1>
+        <p>{cardData?.date}</p>
+        <div className='font-barlow text-justify text-[#6B6B6B]' dangerouslySetInnerHTML={{ __html: cardData?.description }} />
+
+    </div>
+  )
+}
