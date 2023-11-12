53980184ecf168cc579055cacc021f9da55edf2c
Meraj Bighamian <merajbighamian@gmail.com>
2023-11-12 15:32:21 +0330

    feat: fetch tags and categories form backend

--- components/magazine/TagsContainer.tsx
+++ components/magazine/TagsContainer.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
+'use client'
+import React, { useState, useEffect } from 'react';
 
 export default function TagsContainer() {
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_DJANGO_HOST_URL}blog/tags`);
+      const data = await response.json();
+      console.log(data)
+      setTags(data);
+    }
+    fetchTags();
+  }, []);
+
   return (
     <div className="bg-whiteGold px-4 py-8 rounded-sm">
       <span className="border-b-2 border-[#AA84534D] pb-4 font-gilda text-xl tracking-[4px] block">
         Tags
       </span>
       <div className="mt-7 grid grid-cols-3 gap-2">
-        <button className="btn bg-[#D9D9D9] rounded-sm px-1">
-          Health Club
-        </button>
-        <button className="btn bg-[#D9D9D9] rounded-sm px-1">Hotels</button>
-        <button className="btn bg-[#D9D9D9] rounded-sm px-1">
-          Restaurants
-        </button>
-        <button className="btn bg-[#D9D9D9] rounded-sm px-1">Teams</button>
-        <button className="btn bg-[#D9D9D9] rounded-sm px-1">Businesses</button>
+        {tags.map((tag:any) => (
+          <button key={tag.id} className="btn bg-[#D9D9D9] rounded-sm px-2">
+            {tag.title}
+          </button>
+        ))}
       </div>
     </div>
   );
