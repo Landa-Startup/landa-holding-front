8846e49b6ce1b95f101f6e2a4939e8ef1026a37b
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-25 17:06:24 +0330

    chore: add events

---
+++ components/EventCardsContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import EventCard from './EventCard';
+
+export default function EventCardsContainer() {
+  return (
+    <div className="flex flex-col mx-28 my-16 gap-10">
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+    </div>
+  );
+}
