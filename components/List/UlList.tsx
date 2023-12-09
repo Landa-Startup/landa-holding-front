335acb49e8b52e08d9e9facfed3808407abe437c
ali mokhtari <alimokhtari7902@gmail.com>
2023-12-09 10:44:19 +0330

    fix: fixed align landaHolding-block

--- components/List/UlList.tsx
+++ components/List/UlList.tsx
@@ -1,9 +1,23 @@
-export default function UlList({ list, style1, style2, style3 }: { list: string[], style1: string, style2: string, style3: string }) {
+export default function UlList({
+  list,
+  style1,
+  style2,
+  style3
+}: {
+  list: string[];
+  style1: string;
+  style2: string;
+  style3: string;
+}) {
   return (
     <div className={`${style1}`}>
-      <ul className={`w-full list-disc pl-5 font-barlow text-[14px] font-normal text-black ${style2}`}>
+      <ul
+        className={`w-full list-disc pl-5 font-barlow text-[14px] font-normal text-black ${style2}`}
+      >
         {list.map((item, index) => (
-          <li className={`${style3}`} key={index}>{item}</li>
+          <li className={`${style3}`} key={index}>
+            {item}
+          </li>
         ))}
       </ul>
     </div>
--- components/home/CompaniesContainer.tsx
+++ components/home/CompaniesContainer.tsx
@@ -4,15 +4,19 @@ import CompaniesCard from './CompaniesCard';
 // import { companies } from '../../app/[lang]/statics';
 import { useTranslation } from 'app/i18n';
 
-export default async  function CompaniesContainer({lang}:{lang:string}) {
-
-  const { t } = await useTranslation(lang, 'aboutUs')
+export default async function CompaniesContainer({ lang }: { lang: string }) {
+  const { t } = await useTranslation(lang, 'aboutUs');
 
   return (
     <div className="grid grid-cols-2 gap-6 md:grid-cols-2 md:gap-14 lg:grid-cols-5 xl:grid-cols-5 ">
-      {t('companies', {returnObjects: true}).map(({name, logo, link} : {name: string, logo: string, link: string}, index: number) => (
-        <CompaniesCard key={index} name={name} logo={logo} link={link} />
-      ))}
+      {t('companies', { returnObjects: true }).map(
+        (
+          { name, logo, link }: { name: string; logo: string; link: string },
+          index: number
+        ) => (
+          <CompaniesCard key={index} name={name} logo={logo} link={link} />
+        )
+      )}
     </div>
   );
 }
