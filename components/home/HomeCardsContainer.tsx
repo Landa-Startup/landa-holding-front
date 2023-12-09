510c154c732d842d45865efab7a187c81776a320
ali mokhtari <alimokhtari7902@gmail.com>
2023-12-09 12:01:15 +0330

    fix: fixed middel line in multiple language

--- components/home/HomeCardsContainer.tsx
+++ components/home/HomeCardsContainer.tsx
@@ -3,42 +3,41 @@ import HomeCards from './HomeCards';
 import Lines from '../icons/Home/Lines';
 import { CompanySectionsInterface } from '@/types/global';
 import { useTranslation } from 'app/i18n';
-import { dir } from 'i18next'
+import { dir } from 'i18next';
 
-export default async function HomeCardsContainer(
-  { lang }: { lang: string }
-) {
-
-  const { t } = await useTranslation(lang, "mainPage")
-  console.log("lang: ", dir(lang))
+export default async function HomeCardsContainer({ lang }: { lang: string }) {
+  const { t } = await useTranslation(lang, 'mainPage');
+  console.log('lang: ', dir(lang));
   // Define data for the text card containers
 
   return (
-    <div className="relative bg-whiteGold text-black" id="LandaHolding">
-      <div className={dir(lang) == 'rtl' ? 'hidden' : 'block'}>
+    <div className="relative  bg-whiteGold text-black" id="LandaHolding">
+      <div className={dir(lang) == 'rtl' ? 'block' : 'block'}>
         <Lines />
       </div>
       {/* Map over cardData to create HomeTextCardContainers */}
-      {t('cards', { returnObjects: true }).map(({
-        title,
-        text,
-        reverse,
-        index,
-        link,
-        addedClass,
-        images
-      }: CompanySectionsInterface) => (
-        <HomeCards
-          key={index}
-          titles={title}
-          text={text}
-          images={images}
-          reverse={reverse}
-          addedClass={addedClass}
-          link={link}
-          lang={lang}
-        />
-      ))}
+      {t('cards', { returnObjects: true }).map(
+        ({
+          title,
+          text,
+          reverse,
+          index,
+          link,
+          addedClass,
+          images
+        }: CompanySectionsInterface) => (
+          <HomeCards
+            key={index}
+            titles={title}
+            text={text}
+            images={images}
+            reverse={reverse}
+            addedClass={addedClass}
+            link={link}
+            lang={lang}
+          />
+        )
+      )}
     </div>
   );
 }
