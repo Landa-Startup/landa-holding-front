9e0a929b6f95c2411561bea9515975aa0b4cb60d
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-12-09 15:17:43 +0330

    chore: delete entrepreneurs page and update entrepreneur center link

--- app/[lang]/something/page.tsx
+++ app/[lang]/something/page.tsx
@@ -9,16 +9,15 @@ import { useTranslation } from 'app/i18n';
 export const metadata: Metadata = {
   title: 'Landa Holding | Entrepreneurs',
   description:
-    'Explore the Landa Holding Entrepreneurs Form and connect with us to share your entrepreneurial ideas and projects. We are interested in hearing from creative minds and visionaries. Lets collaborate to turn your entrepreneurial dreams into reality.',
+    'Explore the Landa Holding Entrepreneurs Form and connect with us to share your entrepreneurial ideas and projects. We are interested in hearing from creative minds and visionaries. Lets collaborate to turn your entrepreneurial dreams into reality.'
 };
 
 export default async function EntrepreneursPage({
-  params: { lang },
+  params: { lang }
 }: {
   params: { lang: string };
 }) {
-
-  const { t } = await useTranslation(lang, "entrepreneur")
+  const { t } = await useTranslation(lang, 'entrepreneur');
 
   return (
     <div>
