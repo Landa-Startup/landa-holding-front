3276b23bd317a0eab8fe76fe9ee9eac316b2bea9
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-12-09 13:46:41 +0330

    chore: update button href in ButtonRefactor component

--- components/common/ButtonRefactor.tsx
+++ components/common/ButtonRefactor.tsx
@@ -6,23 +6,31 @@ type ButtonProps = {
   text: string;
   type?: 'button' | 'reset' | 'submit' | 'link';
   href?: string;
+  disabled?: boolean;
 };
 
 export default function ButtonRefactor({
   text,
   type = 'button',
-  href
+  href,
+  disabled
 }: ButtonProps) {
   switch (type) {
     // If the type is 'Link'
     case 'link':
       return (
         href && (
-          <Link href={href}>
-            <a className="flex h-[56px] w-[224px] items-center justify-center rounded bg-[#AA8453] p-2 text-white">
-              <span>{text}</span>
-              <ChevronRightIcon className="ml-2 h-5 w-5" />
-            </a>
+          <Link href={href} className="group relative overflow-hidden">
+            <button
+              disabled={disabled}
+              className="flex h-[56px] w-[224px] flex-wrap content-center justify-center rounded bg-[#AA8453] p-2 text-white"
+            >
+              <div className="z-10 flex items-center">
+                <span>{text}</span>
+                <ChevronRightIcon className="ml-2 h-5 w-5" />
+              </div>
+            </button>
+            <span className="absolute inset-0 -translate-x-full rounded bg-black transition-transform duration-500 group-hover:translate-x-0"></span>
           </Link>
         )
       );
@@ -39,7 +47,7 @@ export default function ButtonRefactor({
           >
             <div className="z-10 flex items-center gap-2 rtl:flex-row-reverse">
               <span>{text}</span>
-              <ChevronRightIcon className="h-5 w-5 ltr:ml-2" />
+              <ChevronRightIcon className="h-5 w-5" />
             </div>
           </button>
           <span className="absolute inset-0 -translate-x-full rounded bg-black transition-transform duration-500 group-hover:translate-x-0"></span>
