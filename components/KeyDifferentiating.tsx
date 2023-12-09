942312dffd93dac0445efb8c9e7fe4af07006106
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-12-09 09:38:11 +0330

    chore: refactor KeyDifferentiating component

--- components/KeyDifferentiating.tsx
+++ components/KeyDifferentiating.tsx
@@ -2,11 +2,8 @@ import { useTranslation } from 'app/i18n';
 import Image from 'next/image';
 import React from 'react';
 
-export default async function KeyDifferentiating(
-  { lang }: { lang: string }
-) {
-
-  const { t } = await useTranslation(lang, "aboutUs")
+export default async function KeyDifferentiating({ lang }: { lang: string }) {
+  const { t } = await useTranslation(lang, 'aboutUs');
 
   // t('itemList', {returnObjects: true}).map(({item}: {item: string}, {index}: {index: number}) => {
   //   console.log(item, index);
@@ -22,21 +19,28 @@ export default async function KeyDifferentiating(
         </span>
         <div className="gap-2 font-barlow leading-9 text-black md:w-[350px] lg:w-[450px] xl:w-[550px]">
           <p>
-            {t('KeyDifferentiatingFactors', { returnObjects: true })[0].titleText}
+            {
+              t('KeyDifferentiatingFactors', { returnObjects: true })[0]
+                .titleText
+            }
           </p>
-          <span className="font-bold text-primary">{t('KeyDifferentiatingFactors', { returnObjects: true })[0].textTitle}</span>
-          <ul className='list-decimal ltr:ml-4 rtl:mr-4'>
-            {t('itemList', { returnObjects: true }).map((item: string, index: number) => (
-              <li key={index}>
-                {item}
-              </li>
-            ))}
+          <span className="font-bold text-primary">
+            {
+              t('KeyDifferentiatingFactors', { returnObjects: true })[0]
+                .textTitle
+            }
+          </span>
+          <ul className="list-decimal ltr:ml-4 rtl:mr-4">
+            {t('itemList', { returnObjects: true }).map(
+              (item: string, index: number) => (
+                <li key={index}>{item}</li>
+              )
+            )}
           </ul>
         </div>
       </div>
-      <div className="group mt-10 hidden md:block">
+      <div className="mt-10 hidden md:block">
         <Image
-          className="duration-2000 group-hover:animate-pulse "
           src="/static/images/Home/KeyDifferentiating/Vector 58.png"
           alt="Landa Key Differentiating"
           width={586}
