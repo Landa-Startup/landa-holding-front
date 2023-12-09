eba3cc5a0fe04e73d58e1a94c7ecc6f7371ab63e
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-12-09 12:26:03 +0330

    chore: update text-justify and font styles in page.tsx

--- app/[lang]/acceleration/page.tsx
+++ app/[lang]/acceleration/page.tsx
@@ -60,7 +60,7 @@ export default async function Page({
             />
           </div>
           <p
-            className={`w-[340px] text-justify font-barlow leading-8 tracking-[1.6px] ltr:text-lg rtl:text-base md:h-[248px]  md:w-[550px] lg:w-[680px] lg:pr-2`}
+            className={`w-[340px] text-justify font-barlow ltr:text-lg ltr:leading-6 ltr:tracking-[1.6px] rtl:text-base rtl:leading-[50px] md:h-[248px]  md:w-[550px] lg:w-[680px] lg:pr-2`}
           >
             {t('LandaAccelerator', { returnObjects: true })[0].text}
           </p>
