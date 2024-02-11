8bac925ebfab5601c6b0e535e8fee2ced9bb8e21
ali mokhtari <alimokhtari7902@gmail.com>
2024-02-11 10:34:28 +0330

    fix: fix input required

--- components/investor-registration/InvestorRegistrationForm.tsx
+++ components/investor-registration/InvestorRegistrationForm.tsx
@@ -133,7 +133,6 @@ export default function InvestorRegistrationForm() {
             <CountryInput
               register={register}
               errors={errors}
-              required=""
               nameInput="countryOfResidence"
             />
 
