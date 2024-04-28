53b8e00ed5130d5dd3a256eb2743ec8d5de8f995
barad <dehghanibarad64@gmail.com>
2024-04-28 11:56:05 +0330

    fix: fixed aboutUs

--- components/about/AboutUsLandaHolding.tsx
+++ components/about/AboutUsLandaHolding.tsx
@@ -39,7 +39,7 @@ export default function AboutUsLandaHolding() {
           {t('aboutLandaHolding', { returnObjects: true })[0].text}
         </p>
       </div>
-      <div className='relative object-contain mx-auto h-48 w-96  md:w-[761px] md:h-[419px] md:mt-8 md:mr-32'>
+      <div className='relative object-contain mx-auto h-48 w-96  md:w-[761px] md:h-[419px] md:mt-8 rtl:md:mr-32 md:ltr:ml-32'>
       <Image
         className="brightness-95 contrast-[1.15] drop-shadow-md"
         loading="lazy"
