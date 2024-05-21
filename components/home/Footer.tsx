032e33a6b7810d7778a10f5c79daddae7b06683c
barad <dehghanibarad64@gmail.com>
2024-05-21 13:15:34 +0330

    fix: fixed footer

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -17,10 +17,10 @@ export default async function Footer(
     return currentYear;
   }
   return (
-    <div className="flex flex-col bg-neutral-800 p-8 font-barlow">
+    <div className="flex flex-col bg-[#F7F3EE] p-8 font-barlow">
       <div className="flex flex-col justify-between space-y-5 md:flex-row md:gap-4 md:p-20">
         <div className="mt-5 flex flex-col gap-2 md:w-[344px]">
-          <div className="h-[25px] text-justify text-xl font-medium text-white">
+          <div className="h-[25px] text-justify text-xl font-medium text-[#AA8453]">
             {t('about', { returnObjects: true }).title}
             <br />
           </div>
@@ -28,7 +28,7 @@ export default async function Footer(
             {t('about', { returnObjects: true }).text}
             <br />
           </div>
-          <div className="mt-2 flex h-[22px] w-[126px] flex-row items-center justify-between text-white">
+          <div className="mt-2 flex h-[22px] w-[126px] flex-row items-center justify-between text-neutral-400">
             <Link
               aria-label="Instagram"
               href={
@@ -65,7 +65,7 @@ export default async function Footer(
           </div>
         </div>
         <div className="col-span-1 flex flex-col space-y-1 md:mr-10">
-          <div className="text-justify text-xl font-medium text-white">
+          <div className="text-justify text-xl font-medium text-[#AA8453]">
             {t('explore', { returnObjects: true }).title}
             <br />
           </div>
@@ -97,7 +97,7 @@ export default async function Footer(
           </div>
         </div>
         <div className="col-span-1 flex flex-col space-y-1">
-          <div className="text-xl font-medium text-white">{t('forms', { returnObjects: true }).title}</div>
+          <div className="text-xl font-medium text-[#AA8453]">{t('forms', { returnObjects: true }).title}</div>
           <div className="inline-flex flex-col items-start justify-start self-stretch">
             <Link
               href={'/StartupsForm'}
@@ -126,7 +126,7 @@ export default async function Footer(
           </div>
         </div>
         <div className="col-span-1 flex flex-col space-y-1 md:ml-1 md:w-1/3">
-          <div className="h-[30px] w-[93px]  text-xl font-medium text-white">
+          <div className="h-[30px] w-[93px]  text-xl font-medium text-[#AA8453]">
             {t('contact', { returnObjects: true }).title}
             <br />
           </div>
@@ -136,10 +136,10 @@ export default async function Footer(
           <div dir='ltr' className="text-justify text-base font-normal text-neutral-400">
             {t('contact', { returnObjects: true }).text.cNumber}
           </div>
-          <div className="border-t border-t-white pt-1 text-justify text-base font-normal text-neutral-400">
+          <div className="border-t border-t-[#AA8453] pt-1 text-justify text-base font-normal text-neutral-400">
             {t('contact', { returnObjects: true }).text.tehran}
           </div>
-          <div dir='ltr' className="border-b border-b-white pb-1 text-justify text-base font-normal text-neutral-400">
+          <div dir='ltr' className="border-b border-b-[#AA8453] pb-1 text-justify text-base font-normal text-neutral-400">
             {t('contact', { returnObjects: true }).text.tNumber}
           </div>
           <div className="text-base font-normal text-neutral-400">
@@ -150,9 +150,9 @@ export default async function Footer(
           </div>
         </div>
       </div>
-      <div className="self-stretch border-t pt-4 text-center text-white" dir='ltr'>
+      <div className="self-stretch border-t border-[#AA8453] pt-4 text-center text-[#DDCEBA]" dir='ltr'>
         © Copyright {GetYear()} by{' '}
-        <Link href={'/'} className="text-primary">
+        <Link href={'/'} className="text-[#DDCEBA]">
           LandaHolding
         </Link>
       </div>
